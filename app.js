6b66b3400c17ddafac02ecc1b23b7f4c26f0e0d0
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-08-21 18:19:34 +0530

    Add vercel file to deploy

--- app.js
+++ app.js
@@ -36,12 +36,14 @@ app.use("/api/v1", user);
 app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
-export default app;
-
 app.get("/", (req, res) =>
   res.send(
-    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+    `<h1>Site is Working to work with frontend.</h1>`
   )
 );
+
+export default app;
+
+
 
 app.use(ErrorMiddleware);
