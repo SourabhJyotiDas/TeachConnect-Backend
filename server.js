8de29d3879afcc9abd4697fdffc6317121da98b1
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-09-05 20:09:01 +0530

    new Changes

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ import app from "./app.js";
 import { connectDB } from "./config/database.js";
 import cloudinary from "cloudinary";
 import RazorPay from "razorpay";
-import nodeCron from "node-cron";
-import { Stats } from "./models/Stats.js";
 connectDB();
 
 cloudinary.v2.config({
@@ -17,13 +15,6 @@ export const instance = new RazorPay({
   key_secret: process.env.RAZORPAY_API_SECRET,
 });
 
-nodeCron.schedule("0 0 0 5 * *", async () => {
-  try {
-    await Stats.create({});
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on port: http://localhost:${process.env.PORT}`);
