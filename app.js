ce1e0d71bd8259db4341dde061764eef65112ef5
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-11-30 11:09:11 +0530

    Add Frontend Url to Cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://teachconnect.netlify.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
